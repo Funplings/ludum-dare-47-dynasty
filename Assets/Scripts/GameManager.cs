dcae12d8b100cf4765952bef0cad4d399bda2a31
Imon Husain <imonh222@berkeley.edu>
2020-10-03 14:29:09 -0400

    Setup complete

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance = null;
+
+    private void Awake(){
+        if(instance == null){
+            instance = this;
+        }
+        else if(instance != this){
+            Destroy(this.gameObject);
+        }
+        //Want this to persist throughout the game
+        DontDestroyOnLoad(gameObject);
+    }
+
+    
+}
---
+++ Assets/Scripts/GameState.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+public class GameState
+{
+    int happiness;
+    int money;
+    int food;
+    int soldiers;
+    //currentDynasty
+}
