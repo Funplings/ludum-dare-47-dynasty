6b13e32b97b405beda8386e9a044a3c7a68f2807
Imon Husain <imonh222@berkeley.edu>
2020-10-03 16:24:25 -0400

    Game State additions, added Constants, set up GameManager, begun work on Decisions

---
+++ Assets/Scripts/Decisions/DecisionManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DecisionManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    public void BeginDynasty(){
+        //Hide self and tell map to start
+    }
+}
