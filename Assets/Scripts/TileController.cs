05a2d5bf7e1c35396624e72dc636879eca356174
Matthew Guo <matthew.guo@gmail.com>
2020-10-03 22:03:56 -0700

    tile popup stuff and selection

--- Assets/Scripts/TileController.cs
+++ Assets/Scripts/TileController.cs
@@ -12,12 +12,14 @@ public class TileController: MonoBehaviour
     [SerializeField] Sprite m_PlayerFactionSprite;
     [SerializeField] Sprite[] m_EnemyFactionSprites;
     [SerializeField] GameObject m_TilePopupPrefab;
+    [SerializeField] GameObject m_SelectCoverPrefab;
 
     int m_XIndex;
     int m_YIndex;
     int m_TileType; // 0 = None, 1 = Farm, 2 = Lab, 3 = Mine
     int m_Faction; // -1 = Player faction; -2 = no faction
     GameObject m_CurrSoldier;
+    public GameObject m_SelectCover;
     SpriteRenderer m_SpriteRenderer;
 
     public void Awake() {
@@ -64,25 +66,38 @@ public class TileController: MonoBehaviour
         print(string.Format("Clicked tile [{0}, {1}] of faction {2}", m_XIndex, m_YIndex, m_Faction));
 
         // If this is a player tile, create tile popup (or destroy it, if already selected)
-        if (m_Faction == Constants.PLAYER_FACTION_INDEX) {
+        if (m_Faction == Constants.PLAYER_FACTION_INDEX || true) {
             if (m_CurrSelectedTile == this) {
                 m_CurrSelectedTile = null;
                 Destroy(m_TilePopup);
+                Destroy(m_SelectCover);
                 m_TilePopup = null;
             }
             else {
                 Destroy(m_TilePopup);
+                if (m_CurrSelectedTile != null) {
+                    Destroy(m_CurrSelectedTile.m_SelectCover);
+                }
                 m_CurrSelectedTile = this;
+                m_SelectCover = Instantiate(m_SelectCoverPrefab, transform);
                 m_TilePopup = Instantiate(m_TilePopupPrefab, MapManager.m_StaticPopupCanvas.transform);
+                m_TilePopup.transform.position = transform.position;
 
-                // If this tile is on the top half of the screen, display below; otherwise, display above
+                // Determine vertical position
                 if (m_YIndex >= Constants.NUM_ROWS / 2) {
-                    m_TilePopup.transform.position = transform.position - new Vector3(0, 2f, 0);
+                    m_TilePopup.transform.position -= new Vector3(0, 2f, 0);
                 }
                 else {
-                    m_TilePopup.transform.position = transform.position + new Vector3(0, 2f, 0);
+                    m_TilePopup.transform.position += new Vector3(0, 2f, 0);
                 }
-                
+
+                // Determine horizontal position
+                if (m_XIndex < 2) {
+                    m_TilePopup.transform.position += new Vector3(1.5f, 0, 0);
+                }
+                else if (m_XIndex > Constants.NUM_COLS - 3) {
+                    m_TilePopup.transform.position -= new Vector3(1.5f, 0, 0);
+                }
             }
         }
     }
