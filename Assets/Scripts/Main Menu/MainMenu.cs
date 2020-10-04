d679a9f1132588fb0baec2db8ac797b7b9d8e5e4
Viansa Schmulbach <ansa@berkeley.edu>
2020-10-04 12:15:28 -0700

    add sliding buttons, add input field

--- Assets/Scripts/Main Menu/MainMenu.cs
+++ Assets/Scripts/Main Menu/MainMenu.cs
@@ -18,19 +18,28 @@ public class MainMenu : MonoBehaviour
     [SerializeField] private Button play;
     [SerializeField] private Button tutorial;
 
-    private float[] pos;
-    
     void Start()
     {
         this.r = 0;
         this.g = 0;
         this.b = 0;
         image.color = new Color(r, g, b);
+        play.transform.localPosition = new Vector3(225, -100, 0);
+        tutorial.transform.localPosition = new Vector3(200, -30, 0);
     }
 
     void Update()
     {
-        
+        float ppos = play.transform.localPosition[0];
+        float tpos = tutorial.transform.localPosition[0];
+        if (tpos > 115)
+        {
+            play.transform.localPosition = new Vector3(ppos - 1, -100, 0);
+            tutorial.transform.localPosition = new Vector3(tpos - 1, -30, 0);
+        } else if (ppos > 115)
+        {
+            play.transform.localPosition = new Vector3(ppos - 1, -100, 0);
+        }
 
     }
 
