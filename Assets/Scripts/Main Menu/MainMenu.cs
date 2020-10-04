0239155275f6f1efda6921ee45c0ac4f49676ced
Viansa Schmulbach <ansa@berkeley.edu>
2020-10-03 19:59:23 -0700

    adding sliders

--- Assets/Scripts/Main Menu/MainMenu.cs
+++ Assets/Scripts/Main Menu/MainMenu.cs
@@ -1,22 +1,43 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
     // Start is called before the first frame update
 
-    public static Color selected;
+    public float r;
+    public float g;
+    public float b;
+
+    private Color _colorSelected;
     
     void Start()
     {
-        
+        this.r = 0;
+        this.g = 0;
+        this.b = 0;
     }
-    
-    
-    
+
     public void PlayGame() {
         GameManager.instance.LoadGame();
+    }
+
+
+    public void sliderCallbackR(float value)
+    {
+        r = value;
+    }
+    
+    public void sliderCallbackG(float value)
+    {
+        g = value;
+    }
+    
+    public void sliderCallbackB(float value)
+    {
+        b = value;
     }
     
 }
