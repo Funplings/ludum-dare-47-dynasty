9d6c5353ddd467b6d2dbde3faaf37a88a1929741
Imon Husain <imonh222@berkeley.edu>
2020-10-04 11:09:14 -0400

    Installed DoTween, added Alert

--- Assets/Scripts/Map UI/UIManager.cs
+++ Assets/Scripts/Map UI/UIManager.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using DG.Tweening;
 
 public class UIManager : MonoBehaviour
 {
@@ -16,6 +17,7 @@ public class UIManager : MonoBehaviour
     [SerializeField] private EmpireControl feedControl;
     [SerializeField] private EmpireControl investControl;
     [SerializeField] private RebellionManager rebellion;
+    [SerializeField] private TMP_Text alertText;
     
     public void UpdateHappinessCount(int happiness) {
         m_HappinessInfo.UpdateCount(happiness);
@@ -50,6 +52,15 @@ public class UIManager : MonoBehaviour
     }
 
     public void PlayRound(){
-        rebellion.StartFade();
+        Alert("Pizza");
+        // rebellion.StartFade();
+    }
+
+    public void Alert(string alert){
+        alertText.text = alert;
+        alertText.DOComplete();
+        alertText.transform.DOComplete();
+        alertText.DOFade(1, .5f).OnComplete(() => alertText.DOFade(0, .5f));
+        alertText.rectTransform.DOAnchorPosY(100, 1).OnComplete(() => alertText.rectTransform.anchoredPosition = Vector2.zero);
     }
 }
