fd324a21005f700c03acb470712c68a6ebc4f3a1
Viansa Schmulbach <ansa@berkeley.edu>
2020-10-03 21:28:55 -0700

    add color display

--- Assets/Scripts/Main Menu/MainMenu.cs
+++ Assets/Scripts/Main Menu/MainMenu.cs
@@ -1,6 +1,8 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering;
 using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
@@ -12,32 +14,46 @@ public class MainMenu : MonoBehaviour
     public float b;
 
     private Color _colorSelected;
+    [SerializeField] private Image image;
     
     void Start()
     {
         this.r = 0;
         this.g = 0;
         this.b = 0;
+        image.color = new Color(r, g, b);
+    }
+
+    void Update()
+    {
+        
     }
 
     public void PlayGame() {
         GameManager.instance.LoadGame();
     }
 
+    public void PlayTutorial()
+    {
+        GameManager.instance.LoadTutorial();
+    }
 
     public void sliderCallbackR(float value)
     {
         r = value;
+        image.color = new Color(r, g, b);
     }
     
     public void sliderCallbackG(float value)
     {
         g = value;
+        image.color = new Color(r, g, b);
     }
     
     public void sliderCallbackB(float value)
     {
         b = value;
+        image.color = new Color(r, g, b);
     }
     
 }
