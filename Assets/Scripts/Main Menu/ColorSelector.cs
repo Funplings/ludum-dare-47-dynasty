59f27c327d756a934ae3543955fbacffa71f7c93
Viansa Schmulbach <ansa@berkeley.edu>
2020-10-03 18:51:56 -0700

    adding color selection

---
+++ Assets/Scripts/Main Menu/ColorSelector.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorSelector : MonoBehaviour
+{
+    
+    public void select()
+    {
+        MainMenu.selected = gameObject.GetComponent<Material>().color;
+    }
+
+}
