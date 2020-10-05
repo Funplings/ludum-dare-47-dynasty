63e5a6accceef42ed95746f254b1dfcb3302734a
Imon Husain <imonh222@berkeley.edu>
2020-10-05 14:07:37 -0400

    Win/Lose fixed

--- Assets/Scripts/Win Lose Screen/DynastyListElement.cs
+++ Assets/Scripts/Win Lose Screen/DynastyListElement.cs
@@ -3,15 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using TMPro;
 using UnityEngine;
-using UnityEngine.Assertions.Must;
-using UnityEngine.UI;
 
 public class DynastyListElement : MonoBehaviour
 {
-    
-    public Text text;
-    public void SetText(string info)
+    [SerializeField] TMP_Text number;
+    [SerializeField] TMP_Text name;
+    [SerializeField] TMP_Text timeline;
+
+    public void SetInfo(int number, string name, int yearStart, int yearEnd)
     {
-        text.text = info;
+        this.number.text  = "Dynasty " + number.ToString();
+        this.name.text = string.Format("The <i>{0}</i> Dynasty", name);
+        this.timeline.text = string.Format("Year {0} - Year {1}", yearStart.ToString("D4"), yearEnd.ToString("D4"));
     }
 }
--- Assets/Scripts/Win Lose Screen/ListGenerator.cs
+++ Assets/Scripts/Win Lose Screen/ListGenerator.cs
@@ -5,32 +5,10 @@ using UnityEngine.UI;
 
 public class ListGenerator : MonoBehaviour
 {
-    public string[] names;
-    public int[] durations;
-    public GameObject DynastyInfoTemplate;
-    public void StoreDynastyData()
-    {
-        int length = GameManager.instance.state.m_allDynasties.Count;
-        names = new string[length]; 
-        durations = new int[length];
-        for (int i = 0; i < length; i++)
-        {
-            names[i] = GameManager.instance.state.m_allDynasties[i].GetName();
-            durations[i] = GameManager.instance.state.m_allDynasties[i].GetDuration();
-        }
-    }
+    public GameObject DynastyInfoPrefab;
+    public Transform listParent;
 
-    public void CreateDynastyObj()
-    {
-        for(int i = 0; i < names.Length; i++)
-        {
-            GameObject DynastyInfo = Instantiate(DynastyInfoTemplate) as GameObject;
-            DynastyInfo.SetActive(true);
-            DynastyInfo.GetComponent<DynastyListElement>().SetText("Dynasty Name: " + names[i] + "\n" + "Turns Lasted: " + durations[i]);
-            DynastyInfo.transform.SetParent(DynastyInfoTemplate.transform.parent, false);
-        }
-    }
-    public void CreateTestDynasties()
+    private void CreateTestDynasties()
     {
         Dynasty newtestdynasty = new Dynasty("Jerry", 0, 7);
         GameManager.instance.state.m_allDynasties.Add(newtestdynasty);
@@ -48,14 +26,12 @@ public class ListGenerator : MonoBehaviour
 
     void Start()
     {
-        //CreateTestDynasties();
-        StoreDynastyData();
-        CreateDynastyObj();
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        CreateTestDynasties();
+        for(int i = 0; i < GameManager.instance.state.m_allDynasties.Count; i++){
+            Dynasty dynasty = GameManager.instance.state.m_allDynasties[i];
+            GameObject obj = Instantiate(DynastyInfoPrefab, listParent);
+            DynastyListElement element = obj.GetComponent<DynastyListElement>();
+            element.SetInfo(i+1, dynasty.name, dynasty.turnStarted * Constants.YEARS_PER_TURN, dynasty.turnEnded * Constants.YEARS_PER_TURN);
+        }
     }
 }
