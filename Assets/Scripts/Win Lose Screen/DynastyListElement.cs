c5ebb565bfc84e20399e091f50207a4584ba4ad0
BreadBurner13 <jungkrischan@gmail.com>
2020-10-04 22:46:33 -0700

    finished lose and win screens with scroll implementation

---
+++ Assets/Scripts/Win Lose Screen/DynastyListElement.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using TMPro;
+using UnityEngine;
+using UnityEngine.Assertions.Must;
+using UnityEngine.UI;
+
+public class DynastyListElement : MonoBehaviour
+{
+    
+    public Text text;
+    public void SetText(string info)
+    {
+        text.text = info;
+    }
+}
